5fcc28e29dae25a3e49f7e801fa740ef2713ba13
MiskatAhmmed <memiskat362@gmail.com>
2025-12-01 18:55:41 +0600

    data_transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from network_security.components.data_ingestion import DataIngestion
 from network_security.components.data_validation import DataValidation
+from network_security.components.data_transformation import DataTransformation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -12,7 +13,7 @@ if __name__=='__main__':
         trainingPipelineConfig = TrainingPipelineConfig()
         dataIngestionConfig = DataIngestionConfig(trainingPipelineConfig)
         data_ingestion = DataIngestion(dataIngestionConfig)
-        logging.info("Initiate the data ingestion")
+        logging.info("Initiate the data Ingestion")
         dataIngestionArtifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataIngestionArtifact)
@@ -23,6 +24,13 @@ if __name__=='__main__':
         dataValidationArtifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(dataValidationArtifact)
+
+        dataTransformationConfig = DataTransformationConfig(trainingPipelineConfig)
+        data_transformation = DataTransformation(dataValidationArtifact,dataTransformationConfig)
+        logging.info("Data Transformation Started")
+        dataTransformationArtifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(dataTransformationArtifact)
 
     except Exception as e:
            raise NetworkSecurityException(e,sys)
